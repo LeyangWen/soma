4666eaddf61e774856d847d63686d7ecdf7cb720
leyang <wenleyan@umich.edu>
2024-03-04 16:23:32 -0500

    prepare for slurm

--- slum_mosh_only.sh
+++ slum_mosh_only.sh
@@ -15,6 +15,11 @@
 
 my_job_header
 module load python3.10-anaconda
+module load cuda/11.8.0
+module load cudnn/11.8-v8.7.0
+module load cupti/11.8.0
+module load python/3.10.4
+module load pytorch/2.0.1
 module list
 
 conda activate soma

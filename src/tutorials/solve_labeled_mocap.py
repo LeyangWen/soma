1782b1eb898d96271a23f4f5130e326188eb9f83
leyang <wenleyan@umich.edu>
2024-03-25 13:56:15 -0400

    c3d input change

--- src/tutorials/solve_labeled_mocap.py
+++ src/tutorials/solve_labeled_mocap.py
@@ -5,7 +5,7 @@ import numpy as np
 from loguru import logger
 import argparse
 from soma.amass.mosh_manual import mosh_manual
-
+import time
 
 # todo: get slurm working so that it is faster, test on S1 and another subject
 # todo: remember to add settings.json to include gender info for every subject
@@ -20,12 +20,12 @@ def parse_args():
     # Add the arguments
     parser.add_argument('--soma_work_base_dir', type=str, default='/home/leyang/Documents/soma/SOMA_VEHS', help='The base directory for SOMA work')
     # parser.add_argument('--support_base_dir', type=str, default=None, help='The base directory for support files')
-    parser.add_argument('--mocap_base_dir', type=str, default='/home/leyang/Documents/soma/SOMA_VEHS/evaluation_mocaps/original', help='The base directory for mocap files')
+    parser.add_argument('--mocap_base_dir', type=str, default='/home/leyang/Documents/soma/SOMA_VEHS/support_files/evaluation_mocaps/original', help='The base directory for mocap files')
     # parser.add_argument('--work_base_dir', type=str, default=None, help='The base directory for work files')
-    parser.add_argument('--target_ds_names', nargs='+', default=['SOMA_manual_labeled',], help='Target dataset names')
+    parser.add_argument('--target_ds_names', nargs='+', default=['S02',], help='Target dataset names')
 
     parser.add_argument('--wandb_project', default='Moshpp-VEHS-7M', help='wandb project name')
-    parser.add_argument('--wandb_name', default='Debug', help='wandb run name')
+    parser.add_argument('--wandb_name', default='Run_S1', help='wandb run name')
     parser.add_argument('--slurm_id', default=0, type=int, help='The slurm id of this run')
     parser.add_argument('--debug_mode', action='store_true')  # default: False if store_true
     parser.add_argument('--arg_notes', default="Empty", type=str, help='notes for this run, will be stored in wandb')
@@ -46,13 +46,10 @@ if __name__ == '__main__':
     wandb_run = wandb.init(project=args.wandb_project, name=args.wandb_name, notes=args.arg_notes)
 
     for ds_name in target_ds_names:
-        if args.slurm_id ==0:
-            mocap_fnames = glob(osp.join(mocap_base_dir, ds_name,  '*/*.c3d'))
-        else:  # slurm file structure
-            mocap_fnames = glob(osp.join(mocap_base_dir, ds_name, '*.c3d'))
+        mocap_fnames = glob(osp.join(mocap_base_dir, ds_name,  '*/*.c3d'))
 
         logger.info(f'#mocaps found for {ds_name}: {len(mocap_fnames)}')
-
+        wandb_run.log({'mosh_finished': False, 'render_finished': False, 'time': time.time()})
         mosh_manual(
             mocap_fnames,
             mosh_cfg={
@@ -74,14 +71,48 @@ if __name__ == '__main__':
 
             },
             parallel_cfg={
-                'pool_size': 1,
-                'max_num_jobs': 3,
-                'randomly_run_jobs': True,
+                'pool_size': 10,
+                'max_num_jobs': 10,
+                'randomly_run_jobs': False,
             },
             run_tasks=[
                 'mosh',
-                'render',
             ],
             fast_dev_run=args.debug_mode,
         )
+
+    wandb_run.log({'mosh_finished': True, 'time': time.time()})
+
+    # Render
+    mosh_manual(
+        mocap_fnames,
+        mosh_cfg={
+            'moshpp.verbosity': 1,  # set to 2 to visulaize the process in meshviewer
+            'dirs.work_base_dir': osp.join(work_base_dir, 'mosh_results'),
+            'dirs.support_base_dir': support_base_dir,
+        },
+        render_cfg={
+            'dirs.work_base_dir': osp.join(work_base_dir, 'mp4_renders'),
+            'render.render_engine': 'eevee',  # eevee / cycles,
+            # 'render.render_engine': 'cycles',  # eevee / cycles,
+            'render.show_markers': True,
+            # 'render.save_final_blend_file': True
+            'dirs.support_base_dir': support_base_dir,
+            'dirs.mesh_out_dir': osp.join(work_base_dir, 'meshes'),
+            'dirs.png_out_dir': osp.join(work_base_dir, 'pngs'),
+            'temp_base_dir': osp.join(work_base_dir, 'temp'),
+            # 'render.video_fps': 60
+
+        },
+        parallel_cfg={
+            'pool_size': 10,
+            'max_num_jobs': 10,
+            'randomly_run_jobs': False,
+        },
+        run_tasks=[
+            'render',
+        ],
+        fast_dev_run=args.debug_mode,
+    )
+    wandb_run.log({'mosh_finished': True, 'time': time.time()})
     wandb_run.finish()
